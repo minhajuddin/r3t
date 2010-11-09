0f01a260547f73a5991ee8b8a19cc6f2bf20fdcc
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-11-09 18:20:07 +0530

    configured generators

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,22 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'uri'
+if ENV['MONGOHQ_URL']
+  mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+  ENV['MONGOID_HOST'] = mongo_uri.host
+  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+  ENV['MONGOID_USERNAME'] = mongo_uri.user
+  ENV['MONGOID_PASSWORD'] = mongo_uri.password
+  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
+end
+
 # Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
+require "mongoid/railtie"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -38,10 +49,17 @@ module Lite
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = %w()
 
+    config.generators do |g|
+      g.orm :mongoid
+      g.template_engine :haml
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+    Haml::Template.options[:format] = :html5
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :confirm_password]
   end
 end
